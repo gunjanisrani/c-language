e51832039577e480e9f15c97c770e950e70ab1e2
gunjanisrani <gunjan.i@somaiya.edu>
2024-12-01 08:58:33 +0000

    fourthchange

--- prac3/operator.c
+++ prac3/operator.c
@@ -1,27 +1,39 @@
 #include<stdio.h>
-int main(){
-    int a,b;
+
+int main() {
+    int a, b;
     char c;
+    
     printf("Enter 2 integer numbers\n");
-    scanf("%d%d",&a,&b);
-    printf("Enter the operation you want to perform on them");
-    scanf("%c",&c);
-    switch(c){
+    scanf("%d%d", &a, &b);
+
+    // Consume the newline character left by the previous scanf
+    getchar(); 
+
+    printf("Enter the operation you want to perform on them\n");
+    scanf("%c", &c);
+
+    switch(c) {
         case '+':
-        printf("Sum is %d",(a+b));
-        break;
+            printf("Sum is %d\n", (a + b));
+            break;
         case '-':
-        printf("Difference is %d",(a-b));
-        break;
+            printf("Difference is %d\n", (a - b));
+            break;
         case '*':
-        printf("Multiplication is %d",(a*b));
-        break;
+            printf("Multiplication is %d\n", (a * b));
+            break;
         case '/':
-        printf("Division is %d",(a/b));
-        break;
+            if (b != 0) {
+                printf("Division is %d\n", (a / b));
+            } else {
+                printf("Error: Division by zero is not allowed\n");
+            }
+            break;
         default:
-        printf("Enter valid operator");
-        break;
+            printf("Enter valid operator\n");
+            break;
     }
+    
     return 0;
 }
